805db05cee8f08477f43b9f2d39bd8b4558bfef4
Shahzaibalikhawaja <shahzaib.ali.khawaja@gmail.com>
2024-09-05 23:21:38 +0500

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
+	return (
+		<>
+			<h1 className="bg-green-400 text-black p-4 mb-4 rounded-xl">
+				Tailwind Test
+			</h1>
+    <Card username="Shahzaib" job="Mechatronics Engineer" />
+    <Card/>
 
-<h1 className='bg-green-400 text-black p-4 rounded-xl'>
-  Tailwind Test
-</h1>
-    </>
-  )
+		</>
+	);
 }
 
-export default App
+export default App;
